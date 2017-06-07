d0de3378449c90ae0673448727ed105c1360901a
JensVanAssche <jens.vanassche@student.kdg.be>
2017-06-07 21:05:44 +0200

    testims

--- index.php
+++ index.php
@@ -44,27 +44,21 @@
             <div class="news clearfix">
                 <a href="nieuws.php"><h1><i class="fa fa-bullhorn"></i>Nieuws</h1></a>                
                 <div>
-                    <div>
-                        <a href=""><img src="images/news1.png" alt=""></a>
-                    </div>
+                    <a href=""><img src="images/news1.png" alt=""></a>
                     <a href=""><h1>The Ten Miles are best accessible by foot or by bike</h1></a>
                     <h2>15:03 20/04/2017</h2>
                     <p>Are you coming to the Ten Miles on sunday 23th of april? Or do you have to transport through the city? Better go by foot or take a bike. We'd like to give some tips on how the event is best reached.</p>
                     <a href="">read more</a>
                 </div>
                 <div>
-                    <div>
-                        <a href=""><img src="images/news2.jpg" alt=""></a>
-                    </div>
+                    <a href=""><img src="images/news2.jpg" alt=""></a>
                     <a href=""><h1>10 reasons why you should study at KdG</h1></a>
                     <h2>03:15 19/04/2017</h2>
                     <p>Choosing a college is hard, to help you with your search we compiled a list of 10 reasons why Karel de Grote Hogeschool is a valid choice.</p>
                     <a href="">read more</a>
                 </div>
                 <div>
-                    <div>
-                        <a href=""><img src="images/news3.png" alt=""></a>
-                    </div>
+                    <a href=""><img src="images/news3.png" alt=""></a>
                     <a href=""><h1>Gate 15 will be closed today</h1></a>
                     <h2>7:00 16/04/2017</h2>
                     <p>Gate 15 will be closed on the 16th of april. Tomorrow we'll be back.</p>
@@ -84,22 +78,18 @@
                     <a href="">read more</a>
                 </div>
                 <div>
-                    <div>
-                        <a href=""><img src="images/event2.png" alt=""></a>
-                    </div>
+                    <a href=""><img src="images/event2.png" alt=""></a>
                     <a href=""><h1>Byr + Emil Landman</h1></a>
                     <h2>20:30 25/04/2017 <br />
                     De Roma</h2>
-                    <p>The young, Irish singer/songwriter Bry started his career in the pubs and clubs of Dublin, but decided he wanted to see the world. To keep family and friends up to date he uploaded his adventures to his ...</p>
+                    <p>The young, Irish singer/songwriter Bry started his career in the pubs and clubs of Dublin, but decided he wanted to see the world. To keep family and friends up to date he uploaded his...</p>
                     <a href="">read more</a>
                 </div>
                 <div>
-                    <div>
-                        <a href="https://www.gate15.be/nl/events/kick-off-voor-antwerpse-presidia-6" target="_blank"><img src="images/event3.png" alt=""></a>
-                    </div>
+                    <a href="https://www.gate15.be/nl/events/kick-off-voor-antwerpse-presidia-6" target="_blank"><img src="images/event3.png" alt=""></a>
                     <a href="https://www.gate15.be/nl/events/kick-off-voor-antwerpse-presidia-6" target="_blank"><h1>Kick-off voor Antwerpse presidia</h1></a>
                     <h2>12:30-20:00 11/05/2017</h2>
-                    <p>De kiesweek zit erop, tijd om vooruit te blikken. Hoe verloopt zo'n subsidie- of evenementenaanvraag? Wat als er iets verkeerd gaat? Hoe zet je social media optimaal in om een event te promoten?</p>
+                    <p>De kiesweek zit erop, tijd om vooruit te blikken. Hoe verloopt zo'n subsidie- of evenementenaanvraag? Wat als er iets verkeerd gaat? Hoe zet je social ...</p>
                     <a href="https://www.gate15.be/nl/events/kick-off-voor-antwerpse-presidia-6" target="_blank">read more</a>
                 </div>
             </div>            
