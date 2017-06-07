b99ce523b110812a5e0204695a90317c04ca8487
JensVanAssche <jens.vanassche@student.kdg.be>
2017-06-07 23:40:10 +0200

    uni update

--- php/getIndexContent.php
+++ php/getIndexContent.php
@@ -10,7 +10,7 @@ function getIndexEvents($connect){
 		echo "<div>";
 		echo "<a href='".$row['link']."' target='_blank'><img src='eventImages/".$row['fileName']."' alt=''></a>";
 		echo "</div>";
-		echo "<a href='".$row['link']."' target='_blank'><h1>".$row['title']."</h1></a>";
+		echo "<a href='".$row['link']."' target='_blank'aaaaaa><h1>".$row['title']."</h1></a>";
         echo "<h2>Locatie   ".$row['place']."</h2>";
         echo "<h2>start   ".date('d-m-Y H:i', strtotime($row['eventDate']))."</h2>";
     	echo "<h2>".$row['uid']."</h2>";
@@ -29,9 +29,9 @@ function getIndexNieuws($connect){
 	while($row = mysqli_fetch_assoc($result)){
 		echo "<div>";
 		echo "<div>";
-		echo "<a href='".$row['link']."'><img src='nieuwsImages/".$row['fileName']."' alt=''></a>";
+		echo "<a href='".$row['link']."' target='_blank'><img src='nieuwsImages/".$row['fileName']."' alt=''></a>";
 		echo "</div>";
-		echo "<a href='".$row['link']." target='_blank'><h1>".$row['title']."</h1></a>";
+		echo "<a href='".$row['link']."' target='_blank'><h1>".$row['title']."</h1></a>";
     	echo "<h2>".$row['uID']."</h2>";
     	echo "<h2>".date('d-m-Y H:i', strtotime($row['date']))."</h2>";
     	echo "<p>".nl2br($row['bericht'])."</p>";
